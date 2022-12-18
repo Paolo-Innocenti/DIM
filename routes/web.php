aa3b2170ad65117b527d5087e540fef390bb8391
Paolo Innocenti <p.innocenti@reply.it>
2022-12-18 09:07:14 +0100

    configure test coverage

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,9 @@ use Inertia\Inertia;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/hello', function () {
+    return response('hello-world', 200);
+});
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -27,12 +30,20 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/profile', [ProfileController::class, 'edit'])->name(
+        'profile.edit'
+    );
+    Route::patch('/profile', [ProfileController::class, 'update'])->name(
+        'profile.update'
+    );
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name(
+        'profile.destroy'
+    );
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
